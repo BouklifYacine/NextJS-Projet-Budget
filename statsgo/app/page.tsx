71c0ed8384220639bb0913a3dde013758ae59b90
YacineBoukliff <yacine69800@live.fr>
2025-01-02 17:36:58 +0100

    Redirection sur le bouton de connexion et création du composant boutonconnexion

--- statsgo/app/page.tsx
+++ statsgo/app/page.tsx
@@ -1,7 +1,7 @@
 import { auth } from "@/auth";
-import ShimmerButton from "@/components/ui/shimmer-button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Boutondeconnexion from "@/components/boutondeconnexion";
+import Boutonconnexion from "@/components/boutonconnexion";
 
 export default async function Home() {
   const session = await auth();
@@ -25,23 +25,21 @@ export default async function Home() {
       {session ? (
         session.user?.image ? (
           <div className="flex gap-x-5">
+            
             <Avatar>
               <AvatarImage src={session.user.image} alt="Avatar" />
               <AvatarFallback>
                 {session.user.name?.charAt(0) || "U"}
               </AvatarFallback>
             </Avatar>
+
             <Boutondeconnexion></Boutondeconnexion>
           </div>
         ) : (
           <p className="font-bold text-white">{session.user?.name}</p>
         )
       ) : (
-        <ShimmerButton className="px-4 py-1.5">
-          <span className="text-center leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-base">
-            Connexion
-          </span>
-        </ShimmerButton>
+      <Boutonconnexion></Boutonconnexion>
       )}
     </div>
   );
---
+++ statsgo/components/boutonconnexion.tsx
@@ -0,0 +1,19 @@
+
+import React from 'react'
+import ShimmerButton from "@/components/ui/shimmer-button";
+import Link from 'next/link';
+
+const Boutonconnexion = () => {
+  return (
+    <Link href='/connexion'>
+          <ShimmerButton className="px-4 py-1.5">
+    <span className="text-center leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-base">
+      Connexion
+    </span>
+  </ShimmerButton>
+    </Link>
+  
+  )
+}
+
+export default Boutonconnexion
