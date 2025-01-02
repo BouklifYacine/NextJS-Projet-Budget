6599311d39065c9bcac9c19918522ee390c431da
YacineBoukliff <yacine69800@live.fr>
2025-01-02 18:29:09 +0100

    Finalisation du formulaire d'inscription avec zod et RHF

--- statsgo/components/boutonconnexion.tsx
+++ statsgo/components/boutonconnexion.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 const Boutonconnexion = () => {
   return (
-    <Link href='/connexion'>
+    <Link href='/inscription'>
           <ShimmerButton className="px-4 py-1.5">
     <span className="text-center leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-base">
       Connexion
