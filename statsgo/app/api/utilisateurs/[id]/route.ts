c3142a5dbd628df66b5331de07705ee540e268c9
YacineBoukliff <yacine69800@live.fr>
2025-01-10 09:22:28 +0100

    Suppression d'utilisateurs

--- statsgo/app/api/utilisateurs/[id]/route.ts
+++ statsgo/app/api/utilisateurs/[id]/route.ts
@@ -1,3 +1,4 @@
+import { auth } from "@/auth";
 import { prisma } from "@/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -6,6 +7,11 @@ interface Props {
 }
 
 export async function GET(request: NextRequest, { params }: Props) {
+
+    const session = await auth()
+
+    if(!session) return NextResponse.json("Vous devez etre connecté pour accéder a vos données ")
+
     const { id } = await params; 
 
     const user = await prisma.user.findUnique({
@@ -16,5 +22,45 @@ export async function GET(request: NextRequest, { params }: Props) {
         return NextResponse.json({ error: "Utilisateur non existant" }, { status: 404 });
     }
 
-    return NextResponse.json(user);
+    return NextResponse.json(user.name);
+}
+
+export async function POST(request : NextResponse , {params} : Props){
+    const session = await auth()
+
+    if(!session) return NextResponse.json("Vous devez etre connecté pour poster. ")
+
+     const { id } = await params; 
+
+    const user = await prisma.user.findUnique({
+            where: { id: id } 
+        });
+    
+     if (!user) {
+            return NextResponse.json({ error: "Utilisateur non existant" }, { status: 404 });
+        }
+    
+}
+
+export async function DELETE(request : NextResponse , {params} : Props){
+
+    // const session = await auth()
+
+    // if(!session) return NextResponse.json("Vous devez etre connecté pour poster. ")
+
+     const { id } = await params; 
+
+    const user = await prisma.user.findUnique({
+            where: { id: id } 
+        });
+
+    if (!user) {
+            return NextResponse.json({ error: "Utilisateur non existant" }, { status: 404 });
+        }
+
+    const utilisateursupprimer = await prisma.user.delete({
+        where : { id : id}
+    })
+
+    return NextResponse.json(utilisateursupprimer.name)
 }
