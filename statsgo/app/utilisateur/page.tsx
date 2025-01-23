f169ac1ca3002a20f5c477ebaacc69bbc06f4dd0
YacineBoukliff <yacine69800@live.fr>
2025-01-23 21:43:11 +0100

    Test

--- statsgo/app/utilisateur/page.tsx
+++ statsgo/app/utilisateur/page.tsx
@@ -1,20 +1,29 @@
-import { auth } from '@/auth';
-import { redirect } from 'next/navigation';
+"use server"
 
-const StatsGo = async () => {
-  
-  const session = await auth()
-  const sessionname = session?.user?.name
-  console.log(session)
- 
- 
-    if(!session){
-        redirect("/")
-    }
-    
-  return (
-    <div>Page utilisateur : {sessionname} </div>
-  )
+import { auth } from "@/auth";
+import { prisma } from "@/prisma";
+import { redirect } from "next/navigation";
+
+interface Props {
+  iduser: string;
 }
 
-export default StatsGo
+const StatsGo = async ({ iduser }: Props) => {
+  const session = await auth();
+  const sessionname = session?.user?.name;
+  console.log(session);
+
+  const user = await prisma.user.findUnique({
+    where: { id: iduser ?? ""  },
+  });
+
+  console.log(user?.id)
+
+  if (!session) {
+    redirect("/");
+  }
+
+  return <div>Page utilisateur : {sessionname} </div>;
+};
+
+export default StatsGo;
